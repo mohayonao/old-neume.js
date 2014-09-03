2507d50ed66c82eef93a3de6efd487a9eae9a233
mohayonao <mohayonao@gmail.com>
2014-09-03 09:49:15 +0900

    add _.isBoolean(value)

--- src/utils.js
+++ src/utils.js
@@ -6,4 +6,8 @@ utils.isArray = function(value) {
   return Array.isArray(value);
 };
 
+utils.isBoolean = function(value) {
+  return typeof value === "boolean";
+};
+
 module.exports = utils;
--- test/utils.js
+++ test/utils.js
@@ -11,4 +11,12 @@ describe("utils", function() {
     });
   });
 
+  describe(".isBoolean(value)", function() {
+    it("checks if value is a boolean value", function() {
+      assert(_.isBoolean(true ) === true);
+      assert(_.isBoolean(false) === true);
+      assert(_.isBoolean(null ) === false);
+    });
+  });
+
 });
