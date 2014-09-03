5d738f4410ee927370100155c48d5a4229446efd
mohayonao <mohayonao@gmail.com>
2014-09-03 20:55:33 +0900

    add NeuUnit

---
+++ src/unit.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var _ = require("./utils");
+
+var INIT  = 0;
+var START = 1;
+var STOP  = 2;
+
+function NeuUnit(spec) {
+  this._spec   = spec;
+  this._state  = INIT;
+  this.$outlet = spec.outlet || null;
+}
+
+NeuUnit.prototype.start = function(t) {
+  if (this._state === INIT && _.isFunction(this._spec.start)) {
+    this._state = START;
+    this._spec.start(_.finite(t));
+  }
+};
+
+NeuUnit.prototype.stop = function(t) {
+  if (this._state === START && _.isFunction(this._spec.stop)) {
+    this._state = STOP;
+    this._spec.stop(_.finite(t));
+  }
+};
+
+module.exports = NeuUnit;
