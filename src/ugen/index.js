4de24a637c906a0e015674d30c1113027695cde0
mohayonao <mohayonao@gmail.com>
2014-09-08 16:36:07 +0900

    update ugen/index

--- src/ugen/index.js
+++ src/ugen/index.js
@@ -4,6 +4,10 @@ module.exports = function(neuma) {
   neuma.use(require("./add"));
   neuma.use(require("./biquad"));
   neuma.use(require("./buf"));
+  neuma.use(require("./clip"));
+  neuma.use(require("./comp"));
+  neuma.use(require("./conv"));
+  neuma.use(require("./delay"));
   neuma.use(require("./line"));
   neuma.use(require("./mul"));
   neuma.use(require("./number"));
