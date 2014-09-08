2c1677e698bb3607f73676df2db2f5948511b173
mohayonao <mohayonao@gmail.com>
2014-09-08 17:16:33 +0900

    add ugen/comp

---
+++ src/ugen/comp.js
@@ -0,0 +1,42 @@
+module.exports = function(neuma, _) {
+  "use strict";
+
+  /**
+   * +------------+
+   * | ...inputs  |
+   * +------------+
+   *   |
+   * +-----------------------------+
+   * | DynamicsCompressorNode      |
+   * | - threshold: threshold(-24) |
+   * | - knee: knee(30)            |
+   * | - ratio: ratio(12)          |
+   * | - attack: attack(0.003)     |
+   * | - release: release(0.25)    |
+   * +-----------------------------+
+   *   |
+   */
+  neuma.register("comp", function(ugen, spec, inputs) {
+    var comp = ugen.$context.createDynamicsCompressor();
+
+    comp.threshold.value = 0;
+    comp.knee.value = 0;
+    comp.ratio.value = 0;
+    comp.attack.value = 0;
+    comp.release.value = 0;
+    _.connect({ from: _.defaults(spec.threshold,   -24), to: comp.threshold });
+    _.connect({ from: _.defaults(spec.knee     ,    30), to: comp.knee });
+    _.connect({ from: _.defaults(spec.ratio    ,    12), to: comp.ratio });
+    _.connect({ from: _.defaults(spec.attack   , 0.003), to: comp.attack });
+    _.connect({ from: _.defaults(spec.release  , 0.250), to: comp.release });
+
+    inputs.forEach(function(node) {
+      _.connect({ from: node, to: comp });
+    });
+
+    return new neuma.Unit({
+      outlet: comp
+    });
+  });
+
+};
