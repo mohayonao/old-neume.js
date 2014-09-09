ba9acc577896a757102fabbe5fbeabe55e7d77b0
mohayonao <mohayonao@gmail.com>
2014-09-09 16:36:23 +0900

    add FFT

---
+++ test/fft.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var FFT = require("../src/fft");
+
+describe("FFT", function() {
+
+  describe("#forward(buffer)", function() {
+    it("returns real, imag", function() {
+      var result = FFT.forward(new Float32Array(128));
+
+      assert(result.real instanceof Float32Array);
+      assert(result.real.length === 128);
+      assert(result.imag instanceof Float32Array);
+      assert(result.imag.length === 128);
+    });
+  });
+
+  describe("#inverse(real, imag)", function() {
+    it("returns buffer", function() {
+      var result = FFT.inverse(new Float32Array(128), new Float32Array(128));
+
+      assert(result instanceof Float32Array);
+      assert(result.length === 128);
+    });
+  });
+
+});
