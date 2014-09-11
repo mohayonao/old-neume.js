1e174c9cddc8f28e3a765d775bf38451e3eb9b55
mohayonao <mohayonao@gmail.com>
2014-09-12 08:16:10 +0900

    fix ugen/number

--- src/ugen/number.js
+++ src/ugen/number.js
@@ -2,27 +2,69 @@ module.exports = function(neuma, _) {
   "use strict";
 
   /**
-   * +-------+
-   * | DC(1) |
-   * +-------+
-   *   |
+   * $(number, {
+   *   lag  : number = 0
+   *   curve: number = 0
+   * } ... inputs)
+   *
+   * methods:
+   *   setValue(t, value)
+   *
+   * +--------+      +-------+
+   * | inputs |  or  | DC(1) |
+   * +--------+      +-------+
+   *   ||||||
    * +---------------+
    * | GainNode      |
    * | - gain: value |
    * +---------------+
    *   |
    */
-  neuma.register("number", function(ugen, spec) {
-    var gain = ugen.$context.createGain();
+  neuma.register("number", function(ugen, spec, inputs) {
+    var context = ugen.$context;
 
-    gain.gain.value = _.num(spec.value);
+    var gain  = context.createGain();
+    var data  = _.finite(spec.value);
+    var lag   = _.finite(spec.lag);
+    var curve = _.finite(spec.curve);
 
-    _.connect({ from: new neuma.DC(ugen.$context, 1), to: gain });
+    if (_.isEmpty(inputs)) {
+      inputs = [ new neuma.DC(context, 1) ];
+    }
 
-    return new neuma.Unit({
-      outlet: gain
+    inputs.forEach(function(node) {
+      _.connect({ from: node, to: gain });
     });
 
+    gain.gain.setValueAtTime(data, 0);
+
+    function update(t0, v0, v1, nextData) {
+      if (lag <= 0 || curve < 0 || 1 <= curve) {
+        gain.gain.setValueAtTime(v1, t0);
+      } else {
+        gain.gain.setTargetAtTime(v1, t0, timeConstant(lag, v0, v1, curve));
+      }
+      data = nextData;
+    }
+
+    return new neuma.Unit({
+      outlet: gain,
+      methods: {
+        setValue: function(t, value) {
+          if (_.isFinite(value)) {
+            context.sched(t, function() {
+              update(t, data, value, value);
+            });
+          }
+        }
+      }
+    });
   });
+
+  function timeConstant(duration, startValue, endValue, curve) {
+    var targetValue = startValue + (endValue - startValue) * (1 - curve);
+
+    return -duration / Math.log((targetValue - endValue) / (startValue - endValue));
+  }
 
 };
