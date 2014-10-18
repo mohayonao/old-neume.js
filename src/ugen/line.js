5d687a424eab3a2bb59616a2b55273f066388475
mohayonao <mohayonao@gmail.com>
2014-10-19 08:23:25 +0900

    add $("line") _ syntax

--- src/ugen/line.js
+++ src/ugen/line.js
@@ -25,25 +25,60 @@ module.exports = function(neume, _) {
    *   |
    */
   neume.register("line", function(ugen, spec, inputs) {
-    var startValue = _.finite(_.defaults(spec.start, 1));
-    var endValue   = _.finite(_.defaults(spec.end  , 0));
-    var duration   = _.finite(_.defaults(ugen.$context.toSeconds(spec.dur), 1));
-    return make("linTo", ugen, startValue, endValue, duration, inputs);
+    var list = spec.hasOwnProperty("_") ? makeListFromNumArray(ugen.$context, _.toArray(spec._)) : [
+      _.finite(_.defaults(spec.start, 1)),
+      [
+        _.finite(_.defaults(spec.end, 0)),
+        _.finite(_.defaults(ugen.$context.toSeconds(spec.dur), 1))
+      ]
+    ];
+    return make("linTo", ugen, list, inputs);
   });
 
   neume.register("xline", function(ugen, spec, inputs) {
-    var startValue = Math.max(1e-6, _.finite(_.defaults(spec.start, 1)));
-    var endValue   = Math.max(1e-6, _.finite(_.defaults(spec.end  , 0)));
-    var duration   = _.finite(_.defaults(ugen.$context.toSeconds(spec.dur), 1));
-    return make("expTo", ugen, startValue, endValue, duration, inputs);
+    var list = spec.hasOwnProperty("_") ? makeListFromNumArray(ugen.$context, _.toArray(spec._)) : [
+      _.finite(_.defaults(spec.start, 1)),
+      [
+        _.finite(_.defaults(spec.end, 0)),
+        _.finite(_.defaults(ugen.$context.toSeconds(spec.dur), 1))
+      ]
+    ];
+
+    if (list[0] === 0) {
+      list[0] = 1e-6;
+    }
+
+    for (var i = 1, imax = list.length; i < imax; i++) {
+      if (list[i][0] === 0) {
+        list[i][0] = 1e-6;
+      }
+    }
+
+    return make("expTo", ugen, list, inputs);
   });
 
-  function make(curve, ugen, startValue, endValue, duration, inputs) {
+
+  function makeListFromNumArray(context, list) {
+    var result = [
+      _.finite(list[0])
+    ];
+
+    for (var i = 1, imax = list.length; i < imax; i += 2) {
+      result.push([
+        _.finite(list[i]),
+        _.finite(context.toSeconds(list[i+1]))
+      ]);
+    }
+
+    return result;
+  }
+
+  function make(curve, ugen, list, inputs) {
     var context = ugen.$context;
     var outlet  = null;
 
     var schedId = 0;
-    var param = context.createParam(startValue);
+    var param = context.createParam(list[0]);
 
     if (inputs.length) {
       outlet = context.createGain();
@@ -55,10 +90,14 @@ module.exports = function(neume, _) {
 
     function start(t) {
       var t0 = t;
-      var t1 = t0 + duration;
+      var t1 = t0;
 
-      param.setAt(startValue, t0);
-      param[curve](endValue, t1);
+      param.setAt(list[0], t0);
+
+      for (var i = 1, imax = list.length; i < imax; i++) {
+        t1 += list[i][1];
+        param[curve](list[i][0], t1);
+      }
 
       schedId = context.sched(t1, function(t) {
         schedId = 0;
--- test/ugen/line.js
+++ test/ugen/line.js
@@ -96,6 +96,36 @@ describe("ugen/line", function() {
 
       assert(ended === 0);
     });
+    it("works with _ style", function() {
+      var synth = new Neume(function($) {
+        return $("line", { _: [ 0, 0.5, 0.100, 0.2, 0.100, 0.4, 0.100 ] });
+      })();
+
+      var audioContext = Neume.audioContext;
+      var outlet = synth.toAudioNode().$inputs[0];
+      var ended = 0;
+
+      audioContext.$reset();
+      synth.$context.reset();
+
+      synth.start(0.100).on("end", function(e) {
+        ended = e.playbackTime;
+      });
+
+      audioContext.$processTo("00:00.500");
+
+      assert(closeTo(outlet.gain.$valueAtTime(0.000), 0.000, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.050), 0.000, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.100), 0.000, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.150), 0.250, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.200), 0.500, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.250), 0.350, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.300), 0.200, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.350), 0.300, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.400), 0.400, 1e-2));
+
+      assert(closeTo(ended, 0.400, 1e-2));
+    });
   });
 
   describe("$(xline)", function() {
@@ -137,19 +167,62 @@ describe("ugen/line", function() {
         ended = e.playbackTime;
       });
 
-      audioContext.$processTo("00:00.500");
+      audioContext.$processTo("00:00.350");
 
       assert(closeTo(outlet.gain.$valueAtTime(0.000), 880.000, 1e-2));
       assert(closeTo(outlet.gain.$valueAtTime(0.050), 880.000, 1e-2));
       assert(closeTo(outlet.gain.$valueAtTime(0.100), 880.000, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.125), 806.963, 1e-2));
       assert(closeTo(outlet.gain.$valueAtTime(0.150), 739.988, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.175), 678.572, 1e-2));
       assert(closeTo(outlet.gain.$valueAtTime(0.200), 622.253, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.225), 570.609, 1e-2));
       assert(closeTo(outlet.gain.$valueAtTime(0.250), 523.251, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.275), 479.823, 1e-2));
       assert(closeTo(outlet.gain.$valueAtTime(0.300), 440.000, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.325), 440.000, 1e-2));
       assert(closeTo(outlet.gain.$valueAtTime(0.350), 440.000, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.375), 440.000, 1e-2));
       assert(closeTo(outlet.gain.$valueAtTime(0.400), 440.000, 1e-2));
 
       assert(closeTo(ended, 0.300, 12-2));
+    });
+
+    it("works with _ style", function() {
+      var synth = new Neume(function($) {
+        return $("xline", { _: [ 0, 0.5, 0.100, 0.2, 0.100, 0.4, 0.100 ] });
+      })();
+
+      var audioContext = Neume.audioContext;
+      var outlet = synth.toAudioNode().$inputs[0];
+      var ended = 0;
+
+      audioContext.$reset();
+      synth.$context.reset();
+
+      synth.start(0.100).on("end", function(e) {
+        ended = e.playbackTime;
+      });
+
+      audioContext.$processTo("00:00.450");
+
+      assert(closeTo(outlet.gain.$valueAtTime(0.000), 0.000, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.050), 0.000, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.100), 0.000, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.125), 0.000, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.150), 0.000, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.175), 0.018, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.200), 0.500, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.225), 0.397, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.250), 0.316, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.275), 0.251, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.300), 0.200, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.325), 0.237, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.350), 0.282, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.375), 0.336, 1e-2));
+      assert(closeTo(outlet.gain.$valueAtTime(0.400), 0.400, 1e-2));
+
+      assert(closeTo(ended, 0.400, 1e-2));
     });
   });
 
